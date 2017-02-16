fd8aeffa5ddbefe19b2fcad8a9e1824a16135122
nwre87 <nwre87@gmail.com>
2017-02-16 12:54:52 -1000

    add sequelize, pg-hstore, passport

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+const db = require('./models');
 
-app.use();
+app.listen(PORT, () => {
+  db.sequelize.sync();
+  console.log(`Server running at port: ${PORT}`);
+});
