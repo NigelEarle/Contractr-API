d9872da62526fa62a6b848fcdf22d891d1bba955
nwre87 <nwre87@gmail.com>
2017-02-17 14:49:40 -1000

    add working facebook-oath

--- server.js
+++ server.js
@@ -1,7 +1,46 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
 const db = require('./models');
+const passport = require('passport');
+const Strategy = require('passport-facebook').Strategy;
+
+const CLIENT_ID = process.env.CLIENT_ID || require('./config/facebook.json').ClientId;
+const CLIENT_SECRET = process.env.CLIENT_SECRET || require('./config/facebook.json').ClientSecret;
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Strategy({
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/api/auth/facebook/callback',
+}, (accessToken, refreshToken, profile, cb) => {
+
+  return cb(null, profile);
+}));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+app.get('/', (req,res) => res.redirect('/api/login'));
+app.get('/api/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/api/auth/facebook', passport.authenticate('facebook'));
+app.get('/api/auth/facebook/callback',
+  passport.authenticate('facebook', {failureRedirect: '/api/login'}), (req, res) => {
+    res.json('success!').status(200);
+  });
 
 app.listen(PORT, () => {
   db.sequelize.sync();
