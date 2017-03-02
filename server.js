5a642afd97b6f0e7a8ef217b90e743960d8ee34f
nwre87 <nwre87@gmail.com>
2017-03-01 23:13:18 -1000

    add register route

--- server.js
+++ server.js
@@ -5,14 +5,37 @@ const PORT = process.env.PORT || 3000;
 const db = require('./models');
 const app = express();
 
-// const { User } = db;
+const { User } = db;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/api/register', (req, res) => {
-  // create new user
-  // User.create()
+  const {
+    name,
+    email,
+    picture,
+    token,
+    facebook_id,
+    google_id,
+  } = req.body;
+
+  const payload = {
+    name,
+    email,
+    picture,
+    token,
+    facebook_id: facebook_id ? facebook_id : null,
+    google_id: google_id ? google_id : null,
+  };
+
+  User.create(payload)
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => {
+    res.send(err);
+  });
 });
 
 app.listen(PORT, () => {
