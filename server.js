5abbc32d5f94d8b70e94848cd46c7d8193a2b416
nwre87 <nwre87@gmail.com>
2017-03-01 23:16:33 -1000

    add allow access control headers

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 
 const { User } = db;
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // adjust to only client application IP on prod
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
